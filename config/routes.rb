9ed34749c21f8997df1e808016181e0c0967adce
WalmartFiend <nicknarh@gmail.com>
2014-04-13 13:26:47 -0400

    added project to github from bitbucket

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Blog::Application.routes.draw do
   resources :comments
 
   resources :posts
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
